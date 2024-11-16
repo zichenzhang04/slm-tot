dbabd1b3ed9e67049afc2f94f4da5ebfb7ace16b
Zichen Zhang <zhangzzc@umich.edu>
2024-11-15 21:27:24 -0500

    add spaces to one-step prompt

--- one_step_tot.py
+++ one_step_tot.py
@@ -28,31 +28,31 @@ def generate_prompt(puzzle):
         4 + 8 = 12 (left: 4 6 12)
         6 - 4 = 2 (left: 2 12)
         2 * 12 = 24 (left: 24)
-        Answer: (6 - 4) * (4 + 8) = 24
+        Answer: (6 - 4) * (4 + 8) = 24\n
         Input: 2 9 10 12
         Steps:
         12 * 2 = 24 (left: 9 10 24)
         10 - 9 = 1 (left: 1 24)
         24 * 1 = 24 (left: 24)
-        Answer: (12 * 2) * (10 - 9) = 24
+        Answer: (12 * 2) * (10 - 9) = 24\n
         Input: 4 9 10 13
         Steps:
         13 - 10 = 3 (left: 3 4 9)
         9 - 3 = 6 (left: 4 6)
         4 * 6 = 24 (left: 24)
-        Answer: 4 * (9 - (13 - 10)) = 24
+        Answer: 4 * (9 - (13 - 10)) = 24\n
         Input: 1 4 8 8
         Steps:
         8 / 4 = 2 (left: 1 2 8)
         1 + 2 = 3 (left: 3 8)
         3 * 8 = 24 (left: 24)
-        Answer: (1 + 8 / 4) * 8 = 24
+        Answer: (1 + 8 / 4) * 8 = 24\n
         Input: 5 5 5 9
         Steps:
         5 + 5 = 10 (left: 5 9 10)
         10 + 5 = 15 (left: 9 15)
         15 + 9 = 24 (left: 24)
-        Answer: ((5 + 5) + 5) + 9 = 24
+        Answer: ((5 + 5) + 5) + 9 = 24\n
     '''
 
     system_prompt = (
