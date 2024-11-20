4328d482073a651bf32c97ae2ce1548ba6fd42ef
Shangjun Meng <shangjun@umich.edu>
2024-11-20 00:27:39 -0500

    updated models.py to work with OpenAI python 1.0.0

--- tot/models.py
+++ tot/models.py
@@ -1,23 +1,25 @@
 import os
-import openai
-import backoff 
+import backoff
+from dotenv import load_dotenv
+from openai import OpenAI, OpenAIError
 
 completion_tokens = prompt_tokens = 0
 
+load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY", "")
 if api_key != "":
-    openai.api_key = api_key
+    client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 else:
     print("Warning: OPENAI_API_KEY is not set")
     
-api_base = os.getenv("OPENAI_API_BASE", "")
-if api_base != "":
-    print("Warning: OPENAI_API_BASE is set to {}".format(api_base))
-    openai.api_base = api_base
+# api_base = os.getenv("OPENAI_API_BASE", "")
+# if api_base != "":
+#     print("Warning: OPENAI_API_BASE is set to {}".format(api_base))
+#     openai.api_base = api_base
 
-@backoff.on_exception(backoff.expo, openai.error.OpenAIError)
+@backoff.on_exception(backoff.expo, OpenAIError)
 def completions_with_backoff(**kwargs):
-    return openai.ChatCompletion.create(**kwargs)
+    return client.chat.completions.create(**kwargs)
 
 def gpt(prompt, model="gpt-4", temperature=0.7, max_tokens=1000, n=1, stop=None) -> list:
     messages = [{"role": "user", "content": prompt}]
@@ -30,6 +32,7 @@ def chatgpt(messages, model="gpt-4", temperature=0.7, max_tokens=1000, n=1, stop
         cnt = min(n, 20)
         n -= cnt
         res = completions_with_backoff(model=model, messages=messages, temperature=temperature, max_tokens=max_tokens, n=cnt, stop=stop)
+        res = res.model_dump() # into a dict for ChatComp after 1.0.0
         outputs.extend([choice["message"]["content"] for choice in res["choices"]])
         # log completion tokens
         completion_tokens += res["usage"]["completion_tokens"]
